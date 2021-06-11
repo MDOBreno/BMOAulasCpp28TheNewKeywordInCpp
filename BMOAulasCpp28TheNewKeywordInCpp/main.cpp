28967723b5d17221929001a949c0dc99ff8c9c49
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2021-06-10 23:01:56 -0300

    Initial Commit

---
+++ BMOAulasCpp28TheNewKeywordInCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BMOAulasCpp28TheNewKeywordInCpp
+//
+//  Created by Breno Medeiros on 10/06/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
